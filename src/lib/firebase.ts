a0268c53a76fd7e97ef6bdd0060bbdb550e7f5db
Alahi Majnur Osama <alahimajnur@gmail.com>
2025-08-27 05:49:53 +0000

    FirebaseError: Firebase: Error (auth/network-request-failed).     at cre

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,10 +4,9 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   projectId: "chromashop-f88nq",
   appId: "1:725680619675:web:9a9de9b8f29ddb8cff063e",
-  storageBucket: "chromashop-f88nq.firebasestorage.app",
+  storageBucket: "chromashop-f88nq.appspot.com",
   apiKey: "AIzaSyA88NaKH9xnxElZcv2O1-QSpAkx1kfKQG0",
   authDomain: "chromashop-f88nq.firebaseapp.com",
-  measurementId: "",
   messagingSenderId: "725680619675"
 };
 
